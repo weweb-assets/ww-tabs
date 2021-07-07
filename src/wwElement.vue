b39b1e388b7512e156b0b00baa25af4a51b81adf
Damien DURO <damien@weweb.io>
2021-07-07 18:12:46 +0200

    Version 3.0.0

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,56 +1,54 @@
 <template>
     <div class="tabs-object" :class="content.tabsPosition" :style="cssVariables">
         <div
-            class="tabs-container fixedToTop"
-            ref="fixedTabs"
             v-if="content.fixedToTop && content.tabFields.items"
+            ref="fixedTabs"
+            class="tabs-container fixedToTop"
             :class="content.tabsPosition"
             :style="cssTabsFixedPosition"
         >
             <div
-                class="layout-container"
                 v-for="(field, index) in content.tabFields.items"
                 :key="index"
+                class="layout-container"
                 @click="changeTab(index)"
             >
                 <div class="layout-sublayout">
                     <wwLayout class="layout -layout" :path="`tabsList[${index + 1}]`">
-                        <template v-slot="{ item }">
+                        <template #default="{ item }">
                             <wwLayoutItem>
-                                <wwObject
+                                <wwElement
                                     v-bind="item"
                                     :states="index === currentTabIndex ? ['active'] : []"
-                                ></wwObject>
+                                ></wwElement>
                             </wwLayoutItem>
                         </template>
                     </wwLayout>
                 </div>
             </div>
         </div>
-
-        <div class="tabs-container" :class="content.tabsPosition" v-if="content.tabsList && !content.fixedToTop">
+        <div v-if="content.tabsList && !content.fixedToTop" class="tabs-container" :class="content.tabsPosition">
             <div
-                class="layout-container"
                 v-for="(field, index) in content.tabFields.items"
                 :key="index"
+                class="layout-container"
                 @click="changeTab(index)"
             >
                 <div class="layout-sublayout">
                     <wwLayout class="layout -layout" :path="`tabsList[${index}]`"
-                        ><template v-slot="{ item }">
+                        ><template #default="{ item }">
                             <wwLayoutItem>
-                                <wwObject
+                                <wwElement
                                     v-bind="item"
                                     :states="index === currentTabIndex ? ['active'] : []"
-                                ></wwObject>
+                                ></wwElement>
                             </wwLayoutItem> </template
                     ></wwLayout>
                 </div>
             </div>
         </div>
-
         <transition :name="activeTransition" mode="out-in">
-            <div class="tabs-content" :key="currentTabIndex">
+            <div :key="currentTabIndex" class="tabs-content">
                 <wwLayout
                     class="layout -layout"
                     :class="{ isEditing: isEditing }"
@@ -67,11 +65,12 @@ import { getSettingsConfigurations } from './configuration';
 
 export default {
     props: {
-        content: Object,
+        content: { type: Object, required: true },
         /* wwEditor:start */
-        wwEditorState: Object,
+        wwEditorState: { type: Object, required: true },
         /* wwEditor:end */
     },
+    emits: ['update:content'],
     wwDefaultContent: {
         tabsPosition: 'top',
         transition: 'fade',
@@ -83,7 +82,6 @@ export default {
         fixedToTop: false,
         leftRightPosition: '30%',
         topBottomPosition: '-50%',
-
         tabFields: {
             items: [
                 {
@@ -114,6 +112,34 @@ export default {
             activeTransition: 'fade',
         };
     },
+    computed: {
+        isEditing() {
+            /* wwEditor:start */
+            return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
+            /* wwEditor:end */
+            // eslint-disable-next-line no-unreachable
+            return false;
+        },
+        cssVariables() {
+            return {
+                '--tab-transition-duration': this.content.transitionDuration + 's',
+            };
+        },
+        cssTabsFixedPosition() {
+            return {
+                '--tab-leftRight-position': this.content.leftRightPosition,
+                '--tab-topBottom-position': this.content.topBottomPosition,
+            };
+        },
+        getSublayoutHeight() {
+            const elHeight = document.querySelectorAll('.tabs-sublayout-container');
+            if (elHeight && elHeight.length && elHeight[this.currentTabIndex]) {
+                return `${elHeight[this.currentTabIndex].offsetHeight}px`;
+            }
+
+            return '24px';
+        },
+    },
     watch: {
         'content.tabFields'() {
             if (this.content.tabFields.moveHandler[0] !== null && this.content.tabFields.moveHandler[1] !== null) {
@@ -142,7 +168,7 @@ export default {
                     moveHandler: [null, null],
                 };
 
-                this.$emit('update', {
+                this.$emit('update:content', {
                     tabFields: tabFields,
                     tabsList: tabsList,
                     subTabLayouts: subTabLayouts,
@@ -158,7 +184,7 @@ export default {
                 subTabLayouts.splice(this.content.tabFields.target, 1);
                 tabsContent.splice(this.content.tabFields.target, 1);
 
-                this.$emit('update', {
+                this.$emit('update:content', {
                     tabsList: tabsList,
                     subTabLayouts: subTabLayouts,
                     tabsContent: tabsContent,
@@ -167,34 +193,6 @@ export default {
             }
             this.currentTabIndex = this.content.tabFields.items.findIndex(item => item.checked);
             this.changeTab(this.currentTabIndex);
-        },
-    },
-    computed: {
-        isEditing() {
-            /* wwEditor:start */
-            return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
-            /* wwEditor:end */
-            // eslint-disable-next-line no-unreachable
-            return false;
-        },
-        cssVariables() {
-            return {
-                '--tab-transition-duration': this.content.transitionDuration + 's',
-            };
-        },
-        cssTabsFixedPosition() {
-            return {
-                '--tab-leftRight-position': this.content.leftRightPosition,
-                '--tab-topBottom-position': this.content.topBottomPosition,
-            };
-        },
-        getSublayoutHeight() {
-            const elHeight = document.querySelectorAll('.tabs-sublayout-container');
-            if (elHeight && elHeight.length && elHeight[this.currentTabIndex]) {
-                return `${elHeight[this.currentTabIndex].offsetHeight}px`;
-            }
-
-            return '24px';
         },
     },
     methods: {
@@ -255,7 +253,6 @@ export default {
     flex-direction: column;
     justify-content: center;
     overflow: visible;
-
     &.top {
         flex-direction: column;
     }
@@ -268,14 +265,12 @@ export default {
     &.right {
         flex-direction: row-reverse;
     }
-
     .tabs-content {
         .layout {
             flex-direction: column;
             min-width: 200px;
         }
     }
-
     .tabs-container {
         z-index: 1;
         display: flex;
@@ -286,7 +281,6 @@ export default {
         position: relative;
         width: 100%;
         min-width: 390px;
-
         &.fixedToTop {
             width: 100vw;
             z-index: 9999999;
@@ -299,50 +293,41 @@ export default {
                 left: calc(var(--tab-leftRight-position) - 11%);
             }
         }
-
         &.left {
             flex-direction: column;
             align-items: flex-end;
         }
-
         &.right {
             flex-direction: column;
             align-items: flex-start;
         }
-
         .layout-container {
             display: flex;
             flex-direction: row;
             justify-content: center;
             align-items: center;
-
             .layout-sublayout {
                 flex-direction: row;
                 justify-content: center;
                 align-items: center;
-
                 .layout {
                     flex-direction: column;
                     width: fit-content;
                     height: fit-content;
                     min-width: 80px;
-
                     &:hover {
                         cursor: pointer;
                     }
                 }
-
                 .sublayout {
                     flex-direction: column;
                 }
             }
         }
-
         .tab-dropzone-container {
             width: 100%;
             height: 100%;
             padding: 10px 10px;
-
             .tab_dropzone {
                 width: 100px;
                 height: 100px;
@@ -352,7 +337,6 @@ export default {
 }
 
 // FADE
-
 .fade-enter-active,
 .fade-leave-active {
     transition: opacity var(--tab-transition-duration);
@@ -363,7 +347,6 @@ export default {
 }
 
 // FADE TOP
-
 .fadeTop-enter-active,
 .fadeTop-leave-active {
     transition: all var(--tab-transition-duration);
@@ -378,7 +361,6 @@ export default {
 }
 
 // FADE BOTTOM
-
 .fadeBottom-enter-active,
 .fadeBottom-leave-active {
     transition: all var(--tab-transition-duration);
@@ -393,7 +375,6 @@ export default {
 }
 
 // FADE LEFT
-
 .fadeLeft-enter-active,
 .fadeLeft-leave-active {
     transition: all var(--tab-transition-duration);
@@ -408,7 +389,6 @@ export default {
 }
 
 // FADE RIGHT
-
 .fadeRight-enter-active,
 .fadeRight-leave-active {
     transition: all var(--tab-transition-duration);
